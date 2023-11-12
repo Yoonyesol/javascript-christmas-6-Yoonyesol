fddb96618b7fea6555f8d6c71c254cd588e2c7d3
Yoonyesol <maniac970430@naver.com>
2023-11-13 03:45:01 +0900

    feat(ChristmasEventPlanner): 크리스마스 디데이 할인 금액 계산 기능 구현
    Add:
    - ChristmasEventPlanner.js
        - calculateChristmasDiscount(): 크리스마스 디데이 할인 금액 계산하는 함수

--- src/planner/ChristmasEventPlanner.js
+++ src/planner/ChristmasEventPlanner.js
@@ -5,11 +5,21 @@ class ChristmasEventPlanner {
   #date;
   #orderedList;
   #menuList;
+  #totalAmount;
+  #discount;
 
   constructor(date, order) {
     this.#date = date;
     this.#orderedList = [];
     this.#menuList = menuList;
+    this.#totalAmount = 0;
+    this.#discount = {
+      christmasDiscount: 0,
+      weekdayDiscount: 0,
+      weekendDiscount: 0,
+      specialDiscount: 0,
+      gift: 0,
+    };
     this.#updateOrderedMenu(order);
   }
 
@@ -23,14 +33,19 @@ class ChristmasEventPlanner {
   }
 
   #calculateTotalAmount() {
-    let totalAmount = 0;
-
     this.#orderedList.forEach((item) => {
       const { price, count } = Object.values(item)[0];
-      totalAmount += price * count;
+      this.#totalAmount += price * count;
     });
 
-    return totalAmount;
+    return this.#totalAmount;
+  }
+
+  #calculateChristmasDiscount() {
+    if (this.#date >= 1 && this.#date <= 25) {
+      const christmasDiscount = 1000 + (this.#date - 1) * 100;
+      this.#discount.christmasDiscount = christmasDiscount;
+    }
   }
 
   printOrderedMenu() {
@@ -40,6 +55,10 @@ class ChristmasEventPlanner {
   printTotalAmount() {
     const amount = this.#calculateTotalAmount();
     OutputView.printTotalOrderAmount(amount);
+  }
+
+  calculateDiscount() {
+    this.#calculateChristmasDiscount();
   }
 }
 
