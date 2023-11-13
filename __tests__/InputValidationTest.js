facb8126ee3a9501d1d5bdb2202ec6d8d9d3a366
Yoonyesol <maniac970430@naver.com>
2023-11-14 01:48:46 +0900

    test(InputValidationTest): 주문 포맷 처리 테스트케이스, validateOrder 테스트 케이스 추가
    - validateOrder: 음료만 주문, 20개 초과 메뉴 주문, 20개 메뉴 주문하는 경우 테스트 추가

--- __tests__/InputValidationTest.js
+++ __tests__/InputValidationTest.js
@@ -18,6 +18,11 @@ describe("InputValidation 테스트", () => {
     expect(() =>
       InputValidation.checkValidOrderFormat("초코케이크-2,바비큐립-1")
     ).not.toThrow();
+    expect(() =>
+      InputValidation.checkValidOrderFormat(
+        "초코케이크-2,  바비큐립-1,  레드와인-1"
+      )
+    ).not.toThrow();
     expect(() => InputValidation.checkValidOrderFormat("초코케이크")).toThrow();
     expect(() => InputValidation.checkValidOrderFormat(" ")).toThrow();
     expect(() =>
@@ -57,5 +62,23 @@ describe("InputValidation 테스트", () => {
       { menu: "티본스테이크", count: 2 },
       { menu: "바비큐립", count: 1 },
     ]);
+  });
+
+  test("validateOrder(): 유효하지 않은 주문(음료만 주문)", () => {
+    expect(() =>
+      InputValidation.validateOrder("레드와인-1, 제로콜라-2")
+    ).toThrowError();
+  });
+
+  test("validateOrder(): 유효하지 않은 주문(20개 초과 메뉴 주문)", () => {
+    expect(() =>
+      InputValidation.validateOrder("레드와인-19, 초코케이크-2")
+    ).toThrowError();
+  });
+
+  test("validateOrder(): 유효한 주문(20개 이하의 메뉴 주문)", () => {
+    expect(() =>
+      InputValidation.validateOrder("레드와인-19, 초코케이크-1")
+    ).not.toThrowError();
   });
 });
