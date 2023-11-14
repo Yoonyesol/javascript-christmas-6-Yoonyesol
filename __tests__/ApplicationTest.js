3ddea62db22d951d862e6e9b0b711120486df52f
Yoonyesol <maniac970430@naver.com>
2023-11-14 12:53:28 +0900

    test(ApplicationTest): 날짜, 주문 메뉴 정상 입력에 대한 전체 출력값 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -72,7 +72,8 @@ describe("기능 테스트", () => {
 describe("예외 테스트", () => {
   test("날짜 예외 테스트", async () => {
     // given
-    const INVALID_DATE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    const INVALID_DATE_MESSAGE =
+      "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     const INPUTS_TO_END = ["1", "해산물파스타-2"];
     const logSpy = getLogSpy();
     mockQuestions(["a", ...INPUTS_TO_END]);
@@ -82,12 +83,15 @@ describe("예외 테스트", () => {
     await app.run();
 
     // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(INVALID_DATE_MESSAGE));
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(INVALID_DATE_MESSAGE)
+    );
   });
 
   test("주문 예외 테스트", async () => {
     // given
-    const INVALID_ORDER_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    const INVALID_ORDER_MESSAGE =
+      "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
     const INPUTS_TO_END = ["해산물파스타-2"];
     const logSpy = getLogSpy();
     mockQuestions(["3", "제로콜라-a", ...INPUTS_TO_END]);
@@ -97,6 +101,71 @@ describe("예외 테스트", () => {
     await app.run();
 
     // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(INVALID_ORDER_MESSAGE));
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(INVALID_ORDER_MESSAGE)
+    );
+  });
+
+  test("날짜와 주문 정상 입력에 대한 결과 출력(혜택 적용되지 않음)", async () => {
+    const logSpy = getLogSpy();
+    mockQuestions(["26", "타파스-1,제로콜라-1"]);
+
+    const app = new App();
+    await app.run();
+
+    const expected = [
+      "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!",
+      "<주문 메뉴>",
+      "타파스 1개",
+      "제로콜라 1개",
+      "<할인 전 총주문 금액>",
+      "8,500원",
+      "<증정 메뉴>",
+      "없음",
+      "<혜택 내역>",
+      "없음",
+      "<총혜택 금액>",
+      "0원",
+      "<할인 후 예상 결제 금액>",
+      "8,500원",
+      "<12월 이벤트 배지>",
+      "없음",
+    ];
+
+    expectLogContains(getOutput(logSpy), expected);
+  });
+
+  test("날짜와 주문 정상 입력에 대한 결과 출력(혜택 적용됨)", async () => {
+    const logSpy = getLogSpy();
+    mockQuestions(["3", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"]);
+
+    const app = new App();
+    await app.run();
+
+    const expected = [
+      "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!",
+      "<주문 메뉴>",
+      "티본스테이크 1개",
+      "바비큐립 1개",
+      "초코케이크 2개",
+      "제로콜라 1개",
+      "<할인 전 총주문 금액>",
+      "142,000원",
+      "<증정 메뉴>",
+      "샴페인 1개",
+      "<혜택 내역>",
+      "크리스마스 디데이 할인: -1,200원",
+      "평일 할인: -4,046원",
+      "특별 할인: -1,000원",
+      "증정 이벤트: -25,000원",
+      "<총혜택 금액>",
+      "-31,246원",
+      "<할인 후 예상 결제 금액>",
+      "135,754원",
+      "<12월 이벤트 배지>",
+      "산타",
+    ];
+
+    expectLogContains(getOutput(logSpy), expected);
   });
 });
